90849370ed88b3ca4906bcc14136542618369841
NasikyeCynthia <cynthianasikye@gmail.com>
2024-10-23 19:13:27 +0300

    Add files via upload

---
+++ G2.js
@@ -0,0 +1,8 @@
+// We create a user function that greets users
+function greetUser(name){
+    console.log(`Welcome to Africa ${name}!`)
+}
+
+greetUser("Cynthia")
+greetUser("Alex")
+greetUser("Gabby")
